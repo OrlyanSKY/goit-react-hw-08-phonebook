b453cb3c08f10b4225c6d629612fc7810748e3a0
Denis Orlyansky <000slim001@gmail.com>
2022-12-14 14:47:21 +0200

    fix

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,8 +3,8 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 // import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from 'react-router-dom';
 import ChaletTwoToneIcon from '@mui/icons-material/ChaletTwoTone';
 export default function ButtonAppBar() {
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,8 +3,6 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 
-import Link from '@mui/material/Link';
-
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
